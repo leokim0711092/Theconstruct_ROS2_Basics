4ba9d9f231af90de5fc86099dd10d1eb7176db2f
leokim0711092 <leokim10073@gmail.com>
2023-09-26 07:36:48 +0000

    Unit5 : 5.8 Use of Callback Groups

---
+++ executors_exercises_pkg/src/executor_example_5.cpp
@@ -0,0 +1,53 @@
+#include <rclcpp/rclcpp.hpp>
+#include <unistd.h>
+
+using namespace std::chrono_literals;
+
+class TwoTimer : public rclcpp::Node {
+public:
+  TwoTimer(float sleep_timer1, float sleep_timer2)
+      : Node("slow_timer_subscriber") {
+
+    this->wait_time1 = sleep_timer1;
+    this->wait_time2 = sleep_timer2;
+
+    timer1_ = this->create_wall_timer(
+        500ms, std::bind(&TwoTimer::timer1_callback, this));
+    timer2_ = this->create_wall_timer(
+        500ms, std::bind(&TwoTimer::timer2_callback, this));
+  }
+
+private:
+  void timer1_callback() {
+    sleep(this->wait_time1);
+    RCLCPP_INFO(this->get_logger(), "TIMER 1 CALLBACK");
+  }
+
+  void timer2_callback() {
+    sleep(this->wait_time2);
+    RCLCPP_INFO(this->get_logger(), "TIMER 2 CALLBACK");
+  }
+
+  rclcpp::TimerBase::SharedPtr timer1_;
+  rclcpp::TimerBase::SharedPtr timer2_;
+  float wait_time1;
+  float wait_time2;
+};
+
+int main(int argc, char *argv[]) {
+  rclcpp::init(argc, argv);
+
+  // Instantiate the Node
+  float sleep_time1 = 1.0;
+  float sleep_time2 = 3.0;
+  std::shared_ptr<TwoTimer> two_timer_node =
+      std::make_shared<TwoTimer>(sleep_time1, sleep_time2);
+
+  // Initialize one executor object
+  rclcpp::executors::MultiThreadedExecutor executor;
+  executor.add_node(two_timer_node);
+  executor.spin();
+
+  rclcpp::shutdown();
+  return 0;
+}
